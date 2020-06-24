66ff8910b951db66267683d68b95a89648aec369
Ervin Enriquez <40075841+ervinjenriquez@users.noreply.github.com>
2020-06-23 22:25:56 -0400

    Adding pre-push tests

---
+++ mytest.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+THISPYTHON='python3'
+##################### mytest ########################
+echo "Testing mytest.sh..."
+
+return 0
---
+++ run_tests.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#Manually change this number as you add tests:
+required_passing_tests=1
+tests_passed=0
+
+run_test()
+{
+    sh $1
+    return $?
+}
+
+# Comment out tests you don't want to run. Add additional tests at the bottom.
+# Change required_passing_tests to equal number of tests.
+
+#if run_test 001.test.detectSugar.sh; then tests_passed=$(($tests_passed + 1)); fi
+#if run_test 002.test.PDBSugarID.sh; then tests_passed=$(($tests_passed + 1)); fi
+#if run_test 003.test.InstallationBash.sh; then tests_passed=$(($tests_passed + 1)); fi
+#if run_test 004.test.InstallationPy.sh; then tests_passed=$(($tests_passed + 1)); fi
+#if run_test 005.test.GMMOquery1.sh; then tests_passed=$(($tests_passed + 1)); fi
+#if run_test 006.test.GMMOquery2.sh; then tests_passed=$(($tests_passed + 1)); fi
+#if run_test 007.test.DrawGlycan.sh; then tests_passed=$(($tests_passed + 1)); fi
+if run_test mytest.sh; then tests_passed=$(($tests_passed+1)); fi
+echo "$tests_passed tests passed of $required_passing_tests"
+
+if [[ "$tests_passed" -ge "$required_passing_tests" ]]; then
+    exit 0
+    echo "All tests passed"
+else
+    exit 1
+fi
